3b28538b50bd201a5f24fd8c8a4df0997b81211c
afrojstudent lab <afrojstudent12@gmail.com>
2026-02-03 22:00:52 +0530

    Updating Calculator with new Feature

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-let button = document.querySelectorAll(" .num");
+let button = document.querySelectorAll(".num");
 let input = document.querySelector("input");
-let inputVal = input.value;
-let number = true;
-button.forEach((num)=> {
-    num.addEventListener("click",()=>{
-        if(number){
-            console.log(inputVal);
-            number = num.innerText;
-            input.value = number;
-            console.log(input.value);
+let string = "";
+let str = string
+let arr = Array.from(button);
+arr.forEach((num)=> {
+    num.addEventListener("click",(e)=>{
+        if(e.target.innerText == "=" || e.target.innerText == "Enter"){
+            string = eval(string);
+            input.value = string;
+        }
+        else if (e.target.innerText == "AC"){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerText == "C"){
+            string = string.slice(0,-1);
+            input.value = string;
         }
         else{
-            
+        string+= e.target.innerText;
+        input.value = string;
         }
-
     });
 });
